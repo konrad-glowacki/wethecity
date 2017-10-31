d9d267d3f6a70938cd1dad4ca149b8b665cad519
Bartosz Jędrecki <elitarform@gmail.com>
2017-10-31 18:12:04 +0100

    Add Accounts migration and models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031153306) do
+ActiveRecord::Schema.define(version: 20171031162440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "avatar"
+    t.string "type", null: false
+    t.json "websites"
+    t.string "address", null: false
+    t.string "phone_number"
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "resources", force: :cascade do |t|
     t.string "name", null: false
