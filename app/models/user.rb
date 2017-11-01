537f41834f53c12a99d126e6b0e73aea1fe7da14
Bartosz Jędrecki <elitarform@gmail.com>
2017-11-01 21:34:20 +0100

    Add Projects_Users join table migration, update model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :confirmable, :lockable, :timeoutable, :omniauthable
 
   has_and_belongs_to_many :accounts
+  has_and_belongs_to_many :projects
 
   validates :first_name, presence: true
   validates :last_name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101195619) do
+ActiveRecord::Schema.define(version: 20171101201848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,14 @@ ActiveRecord::Schema.define(version: 20171101195619) do
     t.float "collected_budget"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "projects_users", id: false, force: :cascade do |t|
+    t.integer "project_id", null: false
+    t.integer "user_id", null: false
+    t.string "role", null: false
+    t.datetime "created_at", null: false
+    t.index ["project_id", "user_id"], name: "index_projects_users_on_project_id_and_user_id", unique: true
   end
 
   create_table "resources", force: :cascade do |t|
