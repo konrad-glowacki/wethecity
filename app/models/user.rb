2a275fe2eadab7c893ed6a70fa1b023cbf6d471b
Bartosz Jędrecki <elitarform@gmail.com>
2017-11-01 22:04:46 +0100

    Add role enum to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
 
   has_and_belongs_to_many :accounts
   has_and_belongs_to_many :projects
+  enum role: %i[leader member ambassador]
 
   validates :first_name, presence: true
   validates :last_name, presence: true
