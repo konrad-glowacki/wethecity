53652eb31a96016ccc6c1cb9e63d3a2eb475a02c
Alexander Toropovsky <aleksander.toropowski@gmail.com>
2017-11-03 12:03:20 +0100

    In db/schema.rb modify project_category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171103090158) do
+ActiveRecord::Schema.define(version: 20171103105220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 20171103090158) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_project_categories_on_ancestry"
   end
 
   create_table "resources", force: :cascade do |t|
