11bb435de7715d7c13cd4abac515a1f2b339b07d
Alexander Toropovsky <aleksander.toropowski@gmail.com>
2017-11-03 11:38:33 +0100

    In db/schema.rb add table 'project_categories'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031174049) do
+ActiveRecord::Schema.define(version: 20171103090158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20171031174049) do
     t.integer "account_id", null: false
     t.integer "user_id", null: false
     t.index ["account_id", "user_id"], name: "index_accounts_users_on_account_id_and_user_id", unique: true
+  end
+
+  create_table "project_categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "resources", force: :cascade do |t|
