798b1e20d9dfdd4ab822fe6017a7dc32a20e712c
Bartosz Jędrecki <elitarform@gmail.com>
2017-11-07 11:14:00 +0100

    Remove dropped column

--- db/migrate/20171103114610_change_users_indexes.rb
+++ db/migrate/20171103114610_change_users_indexes.rb
@@ -2,13 +2,11 @@ class ChangeUsersIndexes < ActiveRecord::Migration[5.1]
   def change
     remove_index :users, :confirmation_token
     remove_index :users, :email
-    remove_index :users, :location
     remove_index :users, :reset_password_token
     remove_index :users, :unlock_token
 
     add_index :users, :confirmation_token, unique: true, where: 'deleted_at IS NULL'
     add_index :users, :email, unique: true, where: 'deleted_at IS NULL'
-    add_index :users, :location, where: 'deleted_at IS NULL'
     add_index :users, :reset_password_token, unique: true, where: 'deleted_at IS NULL'
     add_index :users, :unlock_token, unique: true, where: 'deleted_at IS NULL'
   end
