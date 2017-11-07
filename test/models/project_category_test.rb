b9d2aa70309704c1a369f399c046a5ad85adac4a
Alexander Toropovsky <aleksander.toropowski@gmail.com>
2017-11-07 14:22:01 +0100

    Implement ProjectCategoryTest

--- test/models/project_category_test.rb
+++ test/models/project_category_test.rb
@@ -2,8 +2,17 @@
 
 require 'test_helper'
 
+class ProjectCategoryChild < ProjectCategory
+  def ancestry
+    super
+  end
+end
+
 class ProjectCategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Create object which derives from ProjectCategory
+  project_category_child = ProjectCategoryChild.new
+
+  # Check if child object has ancestry
+  expected = project_category_child.has_ancestry
+  assert_equal(expected)
 end
