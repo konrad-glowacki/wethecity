510d47fc3d35e028194df2a3359260a424bcf96b
kayn1 <kayn1@wp.pl>
2017-11-10 14:57:19 +0100

    remove logic from proejcts controller

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -2,39 +2,9 @@
 
 module Admin
   class ProjectsController < Admin::ApplicationController
-    before_action :set_project, only: [:edit, :destroy]
+
     
-    def create
-      @project = Project.new(resource_params)
-      if @project.save
-        flash[:success] = "Project successfully added"
-        redirect_back(fallback_location: admin_projects_path)
-      else
-        flash[:notice] = "Something went wrong"
-        redirect_back(fallback_location: admin_projects_path)
-      end
-    end
-
-    def edit
-      if @project.update_attributes(resource_params)
-        redirect_back(fallback_location: admin_projects_path)
-        flash[:success] = "Project successfully updated"
-      else
-        redirect_to @project
-        flash[:notice] = "Something went wrong"
-      end
-    end
-
-    def destroy
-      @project.destroy
-      redirect_back(fallback_location: admin_projects_path)
-      flash[:notice] = "Project deleted"
-    end
-
-    def set_project
-      @project = Project.find(params[:id])
-    end
-
+    
     def resource_params
       permitted_attributes = dashboard.permitted_attributes + [{ account_ids: [] }]
       custom_params = params.require(:project).permit(permitted_attributes)
