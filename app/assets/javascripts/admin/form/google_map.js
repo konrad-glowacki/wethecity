204affebc5ce9ae750d5a8e8ef0fecbd3da3efe1
kayn1 <kayn1@wp.pl>
2017-11-11 21:13:45 +0100

    set init google maps

--- app/assets/javascripts/admin/form/google_map.js
+++ app/assets/javascripts/admin/form/google_map.js
@@ -1,22 +1,12 @@
-function initMap() {
-  var googleMap = document.getElementById("google-maps");
-};
+ function initMap() {
+  var uluru = {lat: 0, lng: 0};
+  var map = new google.maps.Map(document.getElementById('google-map'), {
+    zoom: 4,
+    center: uluru
+  });
 
-handler = Gmaps.build('Google');
-handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
-  markers = handler.addMarkers([
-    {
-      "lat": 0,
-      "lng": 0,
-      "picture": {
-        "url": "http://people.mozilla.com/~faaborg/files/shiretoko/firefoxIcon/firefox-32.png",
-        "width":  32,
-        "height": 32
-      },
-      "infowindow": "hello!"
-    }
-  ]);
-  handler.bounds.extendWith(markers);
-  handler.fitMapToBounds();
-});
-
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
