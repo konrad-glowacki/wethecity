019c1f2bffe8c4a50f6094d78bc4a38671890993
Konrad Głowacki <konrad@webkrak.pl>
2017-11-13 00:56:05 +0100

    Fixing pipeline

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,18 +3,21 @@
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.enable_processing = false if Rails.env.test?
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_public = true
+    config.fog_provider = 'fog/aws'
+    config.fog_directory = ENV['AWS_BUCKET_NAME']
+    config.fog_use_ssl_for_aws = true
 
-  config.storage = Rails.env.production? ? :fog : :file
-  config.fog_public = true
-  config.fog_provider = 'fog/aws'
-  config.fog_directory = ENV['AWS_BUCKET_NAME']
-  config.fog_use_ssl_for_aws = true
-
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY'],
-    aws_secret_access_key: ENV['AWS_ACCESS_SECRET'],
-    region: ENV['AWS_REGION']
-  }
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_ACCESS_SECRET'],
+      region: ENV['AWS_REGION']
+    }
+  else
+    config.storage = :file
+    config.enable_processing = false if Rails.env.test?
+  end
 end
