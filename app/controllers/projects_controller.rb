e46996fc14f928a254f3869e2597947a51dc003a
Bartosz Jędrecki <elitarform@gmail.com>
2017-11-13 17:40:57 +0100

    Add projects#show, projects#index for testing, fix typo

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.new(name: 'MyName', description_html: 'MyDescription')
+  end
 end
