c5c2abbfd2b46a2c91feb58b46dc93b1e6b5dc14
Fabian Kedzierski <grafska@gmail.com>
2017-11-14 15:46:30 +0100

    Refactor migration

--- db/migrate/20171114135250_add_state_to_engagement.rb
+++ db/migrate/20171114135250_add_state_to_engagement.rb
@@ -1,5 +1,5 @@
 class AddStateToEngagement < ActiveRecord::Migration[5.1]
   def change
-    add_column :engagements, :state, :integer, default: 0
+    add_column :engagements, :state, :integer, default: Engagement.states[:pending]
   end
 end
