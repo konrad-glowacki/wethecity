a35644221c9321da001e9cd8d7f6d9c72a485018
Bartosz Jędrecki <elitarform@gmail.com>
2017-11-17 19:46:49 +0100

    Add missing projects#index route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  resources :projects, constraints: { id: /([0-9]+)/ }, only: [:show]
+  resources :projects, constraints: { id: /([0-9]+)/ }, only: [:show, :index]
 
   namespace :admin do
     resources :projects
