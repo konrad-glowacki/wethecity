9cb5fe7d38c2a46bc19ff794ce77496a3e8e8501
Fabian Kedzierski <grafska@gmail.com>
2017-11-18 17:01:57 +0100

    Add slug to model User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171117160902) do
+ActiveRecord::Schema.define(version: 20171118155728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -176,10 +176,12 @@ ActiveRecord::Schema.define(version: 20171117160902) do
     t.string "provider"
     t.string "uid"
     t.datetime "deleted_at"
+    t.string "slug"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, where: "(deleted_at IS NULL)"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true, where: "(deleted_at IS NULL)"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, where: "(deleted_at IS NULL)"
+    t.index ["slug"], name: "index_users_on_slug", unique: true, where: "(deleted_at IS NULL)"
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, where: "(deleted_at IS NULL)"
   end
 
