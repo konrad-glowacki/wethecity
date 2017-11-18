c057248a30fbbfad4a4bd96eb4fc06e781e08375
Fabian Kedzierski <grafska@gmail.com>
2017-11-18 17:41:38 +0100

    Add slug to model Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118160215) do
+ActiveRecord::Schema.define(version: 20171118163450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,9 +45,11 @@ ActiveRecord::Schema.define(version: 20171118160215) do
     t.datetime "updated_at", null: false
     t.string "ancestry"
     t.datetime "deleted_at"
+    t.string "slug"
     t.index ["ancestry"], name: "index_categories_on_ancestry"
     t.index ["deleted_at"], name: "index_categories_on_deleted_at"
     t.index ["name"], name: "index_categories_on_name", unique: true, where: "(deleted_at IS NULL)"
+    t.index ["slug"], name: "index_categories_on_slug", unique: true, where: "(deleted_at IS NULL)"
   end
 
   create_table "categories_projects", id: false, force: :cascade do |t|
