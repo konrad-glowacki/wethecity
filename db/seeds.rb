2b47f07eadeea22bb55890a25ead5444d1991ce5
kayn1 <kayn1@wp.pl>
2017-11-22 00:34:39 +0100

    retype dir name

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Super Admins
-SuperAdmin.create(name: 'Admin', email: 'admin@wethe.city', password: 'smartcitykrakow')
+SuperAdmin.create(name: 'Admin', email: 'admin1@wethe.city', password: 'smartcitykrakow1')
 
 # Categories
 # Category.create(name: )
