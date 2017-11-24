f3c07473c2220b782d93e4f996e4930a99b21285
kayn1 <kayn1@wp.pl>
2017-11-24 22:50:43 +0100

    organisation controller spec with data from factory

--- spec/controllers/organisations_controller_spec.rb
+++ spec/controllers/organisations_controller_spec.rb
@@ -2,10 +2,26 @@ require 'rails_helper'
 
 RSpec.describe OrganisationsController, type: :controller do
 
-  describe "GET #show" do
+  describe "GET #show" do    
     it "returns http success" do
       get :show
       expect(response).to render_template('show')
+    end
+
+    describe "responds with organization" do 
+      before do
+        organization = FactoryBot.build_stubbed!(:organization)
+      end
+
+      it "returns organisation" do 
+      	get :show, id: organization.id
+        expect(organization).to be_truthy
+      end
+
+      it "returns organization's projects" do 
+        get :show, id: organization.id
+        expect(organization).to be_truthy
+      end
     end
   end
 
