979333b6ba8f7f406ef87239c946cfafbf41a033
Fabian Kedzierski <grafska@gmail.com>
2017-11-24 14:00:46 +0100

    Modify decorator spec

--- spec/decorators/projects_spec.rb
+++ spec/decorators/projects_spec.rb
@@ -4,25 +4,28 @@ require 'rails_helper'
 
 RSpec.describe ProjectDecorator, type: :decorators do
   describe 'Project decorator' do
-    fixtures :projects
-
     it 'return 100 when required_budget equal collected_budget' do
-      project = projects(:project1).decorate
+      project = build_stubbed(:project, required_budget: 200, collected_budget: 200).decorate
       expect(project.budget_completed_percentage).to eq(100)
     end
 
     it 'return value when required_budget different than collected_budget' do
-      project = projects(:project2).decorate
-      expect(project.budget_completed_percentage).to eq(27)
+      project = build_stubbed(:project, required_budget: 200, collected_budget: 100).decorate
+      expect(project.budget_completed_percentage).to eq(50)
     end
 
-    it 'return 0 when collected_budget is 0' do
-      project = projects(:project_collected_budget_nil).decorate
-      expect(project.budget_completed_percentage).to eq(0)
+    it 'return nil when collected_budget is nil' do
+      project = build_stubbed(:project, required_budget: 200, collected_budget: nil).decorate
+      expect(project.budget_completed_percentage).to eq(nil)
     end
 
-    it 'return 0 when required_budget is 0' do
-      project = projects(:project_required_budget_nil).decorate
+    it 'return nil when required_budget is nil' do
+      project = build_stubbed(:project, required_budget: nil, collected_budget: 200).decorate
+      expect(project.budget_completed_percentage).to eq(nil)
+    end
+
+    it 'return nil when required_budget and collected_budget are nil' do
+      project = build_stubbed(:project, required_budget: nil, collected_budget: nil).decorate
       expect(project.budget_completed_percentage).to eq(nil)
     end
   end
