d2c58e995c45783f1e5590f184ae0d9ffa0cc490
kayn1 <kayn1@wp.pl>
2017-11-24 16:29:54 +0100

    convert tab into spaces

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,6 +1,6 @@
 class OrganisationsController < ApplicationController
   def show
-  	@organisation = Organisation.friendly.find(params[:id])
-  	@projects = @organisation.projects
+    @organisation = Organisation.friendly.find(params[:id])
+    @projects = @organisation.projects
   end
 end
