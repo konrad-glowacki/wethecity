e7af75e71bd5154c025a7cca0ec093bc704c9ff5
Damian Mamla <damian.mamla@gmail.com>
2017-11-25 13:13:44 +0100

    Refactoring v2

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,19 @@ module Users
   class RegistrationsController < Devise::RegistrationsController
     respond_to :js
 
+    def create
+      super
+      return unless resource.errors.empty?
+      resource.skip_confirmation!
+      sign_in resource
+    end
+
+    protected
+
+    def update_resource(resource, params)
+      resource.update_without_password(params)
+    end
+
     private
 
     def sign_up_params
