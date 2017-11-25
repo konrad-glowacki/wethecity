5d101cf326028dd8d319fafb0fd29a381da85364
Damian Mamla <damian.mamla@gmail.com>
2017-11-25 12:09:13 +0100

    Refactoring

--- Gemfile
+++ Gemfile
@@ -111,9 +111,6 @@ group :development, :test do
   # Load environment variables from .env
   gem 'dotenv-rails'
 
-  # Email preview
-  gem 'letter_opener'
-
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   # Adds support for Capybara system testing and selenium driver
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,13 +4,6 @@ module Users
   class RegistrationsController < Devise::RegistrationsController
     respond_to :js
 
-    def init
-      @user = build_resource(sign_up_params)
-      session[:user][:password] = @user.password
-      session[:user][:password_confirmation] = @user.password_confirmation
-      session[:user][:email] = @user.email
-    end
-
     private
 
     def sign_up_params
