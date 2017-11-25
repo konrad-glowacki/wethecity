bfc4ac979c1d50ac74a61e24615adce5d95abc91
kayn1 <kayn1@wp.pl>
2017-11-25 12:04:52 +0100

    cleanup organisation controller spec

--- spec/controllers/organisations_controller_spec.rb
+++ spec/controllers/organisations_controller_spec.rb
@@ -7,22 +7,6 @@ RSpec.describe OrganisationsController, type: :controller do
       get :show
       expect(response).to render_template('show')
     end
-
-    describe "responds with organization" do 
-      before do
-        let(:organization) { FactoryBot.build_stubbed(:organization) } 
-      end
-
-      it "returns organisation" do 
-      	get :show, id: organization.id
-        expect(organization).to be_truthy
-      end
-
-      it "returns organization's projects" do 
-        get :show, id: organization.id
-        expect(organization).to be_truthy
-      end
-    end
   end
 
 end
